fd3f48bb6c820cc04f411a55735441a9dc1b5530
FabiOquendo <fabioquendo95@gmail.com>
2025-07-28 21:01:21 -0400

    Creacion de la clase persona y se mueven metodos de escritura

--- src/CuentaCredito.java
+++ src/CuentaCredito.java
@@ -1,32 +1,7 @@
-import java.util.Scanner;
-
 public class CuentaCredito {
     public double cupo;
     public int cuotas;
     
-    public static String ingresarTexto(String mensaje) {
-        String texto;
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(mensaje);
-        texto = scanner.nextLine();
-        return texto;
-    }
-    
-    public static int ingresarEntero(String mensaje) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(mensaje);
-        int valorEntero = scanner.nextInt();
-        return valorEntero;
-
-    }
-    
-    public static double ingresarRealDouble(String mensaje) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(mensaje);
-        double valorReal = scanner.nextFloat();
-        return valorReal;
-    }
-
     public void pagar(double montoPagar){
         if(cupo>montoPagar) {
             System.out.println("Pagando");
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,42 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Persona miguel = new Persona();
+
+        CuentaAhorros cuentaAhorrosMiguel = new CuentaAhorros();
+
+        miguel.cedula = ingresarEntero("Ingrese la cedula");
+        miguel.nombre = ingresarTexto("Ingrese el nomnbre");
+        miguel.apellido = ingresarTexto("Ingrese el apellido");
+        miguel.CuentaAhorros = cuentaAhorrosMiguel;
+        miguel.CuentaAhorros.saldo = ingresarRealDouble("Ingrese el saldo de su cuenta");
+
+        System.out.print(miguel.nombre + " " + miguel.apellido + " con cedula numero : ");
     }
+
+    public static String ingresarTexto(String mensaje) {
+        String texto;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(mensaje);
+        texto = scanner.nextLine();
+        return texto;
+    }
+    
+    public static int ingresarEntero(String mensaje) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(mensaje);
+        int valorEntero = scanner.nextInt();
+        return valorEntero;
+
+    }
+    
+    public static double ingresarRealDouble(String mensaje) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(mensaje);
+        double valorReal = scanner.nextFloat();
+        return valorReal;
+    }
+
 }
