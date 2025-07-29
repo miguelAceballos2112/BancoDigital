c527d7d43ae279f86392a753dfb563ebcd198d51
Miguel Angel <miguelceballos2112@gmail.com>
2025-07-28 19:44:58 -0500

    modificacion de la clase CuentaCredito XDXD

--- src/CuentaCredito.java
+++ src/CuentaCredito.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class CuentaCredito {
     public double cupo;
     public int cuotas;
+    
+    public static String ingresarTexto(String mensaje) {
+        String texto;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(mensaje);
+        texto = scanner.nextLine();
+        return texto;
+    }
+    
+    public static int ingresarEntero(String mensaje) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(mensaje);
+        int valorEntero = scanner.nextInt();
+        return valorEntero;
+
+    }
+    
+    public static double ingresarRealDouble(String mensaje) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(mensaje);
+        double valorReal = scanner.nextFloat();
+        return valorReal;
+    }
 
     public void pagar(double montoPagar){
         if(cupo>montoPagar) {
