c8c21be87135d20fedd6e064085d562a0afdedf4
FabiOquendo <fabioquendo95@gmail.com>
2025-07-28 21:02:10 -0400

    Creacion de la clase persona

---
+++ src/Persona.java
@@ -0,0 +1,9 @@
+public class Persona {
+
+    public int cedula;
+    public String nombre;
+    public String apellido;
+
+    public CuentaAhorros cuentaAhorros;
+    public CuentaCredito tarjetaCredito;
+}
