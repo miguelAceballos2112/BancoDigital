a300e8e5000dafd12e1f728486e8fb679ce24427
FabiOquendo <fabioquendo95@gmail.com>
2025-08-02 17:28:34 -0400

    Geters and seters for Person

--- src/Models/Persona.java
+++ src/Models/Persona.java
@@ -2,12 +2,54 @@ package Models;
 
 public class Persona {
 
-    public int cedula;
-    public String nombre;
-    public String apellido;
+    private int cedula;
+    private String nombre;
+    private String apellido;
 
-    public CuentaAhorros cuentaAhorros;
-    public CuentaCredito tarjetaCredito;
+    private CuentaAhorros cuentaAhorros;
+    private CuentaCredito tarjetaCredito;
+
+    // Getter
+    public int getCedula() {
+        return this.cedula;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getApellido() {
+        return this.apellido;
+    }
+
+    public CuentaAhorros getCuentaAhorros() {
+        return this.cuentaAhorros;
+    }
+
+    public CuentaCredito getTarjetaCredito() {
+        return this.tarjetaCredito;
+    }
+
+    // Setter
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setCuentaAhorros(CuentaAhorros cuentaAhorros) {
+        this.cuentaAhorros = cuentaAhorros;
+    }
+
+    public void setCuentaCredito(CuentaCredito cuentaCredito) {
+        this.tarjetaCredito = cuentaCredito;
+    }
 
     public Persona(int cedula, String nombre, String apellido) {
         this.cedula = cedula;
