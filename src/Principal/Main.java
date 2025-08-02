ced73e17969e971c544f229a0ca0ef14d260128a
FabiOquendo <fabioquendo95@gmail.com>
2025-08-02 17:16:12 -0400

    Organizacion del main

--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -14,32 +14,26 @@ public class Main {
         int cedula = ingresarEntero("Ingrese la cedula: ");
         String nombre = ingresarTexto("Ingrese el nombre: ");
         String apellido = ingresarTexto("Ingrese el apellido: ");
-
         Persona miguel = new Persona(cedula, nombre, apellido);
 
         // Datos cuenta ahorro
-
         int numeroCuentaAhorros = ingresarEntero("Ingrese el numero de la cuenta de ahorros: ");
-        NumberFormat formatoMoneda = NumberFormat.getCurrencyInstance(new Locale("es", "CO"));
-        CuentaAhorros cuentaAhorrosMiguel = new CuentaAhorros(numeroCuentaAhorros,
-                miguel.nombre + " " + miguel.apellido);
+        CuentaAhorros cuentaAhorrosMiguel = new CuentaAhorros(numeroCuentaAhorros, miguel.nombre + " " + miguel.apellido);
         miguel.cuentaAhorros = cuentaAhorrosMiguel;
-        String saldoFormateado = formatoMoneda.format(miguel.cuentaAhorros.saldo);
-
+        
         // Datos tarjeta de credito
-
         int numeroCuentaCredito = ingresarEntero("Ingrese el numero de la tarjeta de credito: ");
         double cupo = ingresarRealDouble("Ingrese el cupo de su tarjeta de credito: ");
-        NumberFormat formatoMonedaC = NumberFormat.getCurrencyInstance(new Locale("es", "CO"));
-        CuentaCredito cuentaCreditoMiguel = new CuentaCredito(numeroCuentaCredito,
-                miguel.nombre + " " + miguel.apellido, cupo);
+        CuentaCredito cuentaCreditoMiguel = new CuentaCredito(numeroCuentaCredito, miguel.nombre + " " + miguel.apellido, cupo);
         miguel.tarjetaCredito = cuentaCreditoMiguel;
-        String cupoFormateado = formatoMonedaC.format(miguel.tarjetaCredito.cupo);
-
+        
         // Presentamos info por consola
         System.out.println(miguel.nombre + " " + miguel.apellido + " con cedula numero : " + miguel.cedula + ".");
+        NumberFormat formatoMoneda = NumberFormat.getCurrencyInstance(new Locale("es", "CO"));
+        String saldoFormateado = formatoMoneda.format(miguel.cuentaAhorros.saldo);
         System.out.println("Tiene una cuenta de ahorros con numero: " + miguel.cuentaAhorros.numeroCuenta
                 + " con un saldo de: " + saldoFormateado + ".");
+        String cupoFormateado = formatoMoneda.format(miguel.tarjetaCredito.cupo);
         System.out.println("Tiene una tarjeta de credito con numero: " + miguel.tarjetaCredito.numeroCuenta
                 + " con un cupo de: " + cupoFormateado + ".");
 
